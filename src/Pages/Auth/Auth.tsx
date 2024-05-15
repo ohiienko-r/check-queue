2cd73829d21cb58768b606de058eddb92e3ad607
Roman Ohiienko <ohiienko.r@gmail.com>
2024-05-15 16:49:24 +0300

    feat: implement log in function call on form submit

--- src/Pages/Auth/Auth.tsx
+++ src/Pages/Auth/Auth.tsx
@@ -1,26 +1,35 @@
-import { FC } from "react";
-import { Button } from "@/components";
+import { FC, FormEvent } from "react";
+import { logIn } from "@/Firebase";
 import classes from "./Auth.module.scss";
 
 const Auth: FC = () => {
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    await logIn({
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    });
+  };
+
   return (
     <div className={classes.container}>
       <h3>Sign In</h3>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">
             Email<span>*</span>:
           </label>
-          <input id="email" type="email" />
+          <input id="email" type="email" name="email" />
         </div>
         <div>
           <label htmlFor="password">
             Password<span>*</span>:
           </label>
-          <input id="password" type="password" />
+          <input id="password" type="password" name="password" />
         </div>
+        <button type="submit">Sign In</button>
       </form>
-      <Button innerText="Sign In" onPress={() => console.log("Nice try bro")} />
     </div>
   );
 };
