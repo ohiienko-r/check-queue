18b45c278add0077bd06c0920f07ff250fd253e2
Roman Ohiienko <ohiienko.r@gmail.com>
2024-05-16 20:27:48 +0300

    feat:complete user and mail section. fix:  change layot

--- src/Root/Root.tsx
+++ src/Root/Root.tsx
@@ -1,11 +1,14 @@
-import Header from "./Header/Header";
-import MainSection from "./MainSection/MainSection";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar/Sidebar";
+import classes from "./Root.module.scss";
 
 const Root = () => {
   return (
     <>
-      <Header />
-      <MainSection />
+      <Sidebar />
+      <main className={classes.mainContainer}>
+        <Outlet />
+      </main>
     </>
   );
 };
